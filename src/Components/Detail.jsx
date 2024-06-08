2cdaf86930ed0a09f2bf31ad53837e47748ab3bd
brenda-soto <bsoto@pursuit.org>
2024-06-08 11:23:06 -0400

    Components and pages added

---
+++ src/Components/Detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WhatIf from "./WhatIf";
+
+const Detail = () => {
+  return (
+    <div>
+      <WhatIf />l
+    </div>
+  );
+};
+
+export default Detail;
---
+++ src/Components/WhatIf.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WhatIf = () => {
+  return <div>WhatIf</div>;
+};
+
+export default WhatIf;
