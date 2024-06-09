fb192b2a543f5b5874418a04dd755553847c9af7
brenda-soto <bsoto@pursuit.org>
2024-06-08 22:58:05 -0400

    Moved all keys and urls to .env and replaced with variables

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,51 +1,53 @@
-
 import axios from "axios";
 import WhatIf from "./WhatIf";
 import { useEffect, useState } from "react";
 
+const searchURL = import.meta.env.VITE_BASE_SEARCH_URL;
 
-
-const Detail = ({title}) => {
+const Detail = ({ title }) => {
   const [worldNews, setWorldNews] = useState([]);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
+
   useEffect(() => {
     const fetchWorldNews = async () => {
-      const url = `https://api.worldnewsapi.com/search-news?text=${title}`;
-      const apiKey = '516b78b5bbe34582af84ae35bbdb1a50';
+      const url = `${searchURL}?text=${title}`;
+      const apiKey = import.meta.env.VITE_SEARCH_API_KEY;
 
       try {
         const response = await axios.get(url, {
           headers: {
-            'x-api-key': apiKey,
+            "x-api-key": apiKey,
           },
         });
         // const newsWithIds = response.data.news.map(news => ({ ...news, id: uuidv4() }));
         setWorldNews(response.data.news);
         setError(null);
       } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error("There was a problem with the fetch operation:", error);
         setError("Failed to fetch world news. Please try again later.");
       }
     };
 
     fetchWorldNews();
-  }, [])
+  }, []);
+
+  // Write another fetch call with the URL from the article returned in worldnews.news[0].url. This will be the variable used in fetch
+
   return (
     <div>
-      <div>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      <ul>
-        {worldNews.map((newsItem, index) => (
-          <li key={index}>
-            <h3>{newsItem.title}</h3>
-            <p>{newsItem.description}</p>
-            {/* <a href={newsItem.url} target="_blank" rel="noopener noreferrer">Read more</a> */}
-          </li>
-        ))}
-      </ul>
-    </div>
-
       <WhatIf />
+      <div>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        <ul>
+          {worldNews.map((newsItem, index) => (
+            <li key={index}>
+              <h3>{newsItem.title}</h3>
+              <p>{newsItem.description}</p>
+              {/* <a href={newsItem.url} target="_blank" rel="noopener noreferrer">Read more</a> */}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Components/WorldNewsTest.jsx
+++ src/Components/WorldNewsTest.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-const url =
-  "https://api.worldnewsapi.com/search-news?text=earth+quake&language=en&earliest-publish-date=2024-04-01";
-// "https://api.worldnewsapi.com/extract-news?url=https://nypost.com/2024/04/02/us-news/lawmakers-demand-to-know-if-taxpayers-funded-al-shabaab-terrorist/";
-const apiKey = "bc9aa6400b3b4925aca424cf05969a54";
+
+const extractURL = import.meta.env.VITE_BASE_EXTRACT_URL;
+const extractKey = import.meta.env.VITE_WORLD_API_KEY;
 
 const WorldNewsTest = () => {
   const [article, setArticle] = useState();
@@ -12,7 +11,7 @@ const WorldNewsTest = () => {
   };
 
   // useEffect(() => {
-  //   fetch(url, {
+  //   fetch(extractURL, {
   //     method: "GET",
   //     headers: {
   //       "x-api-key": apiKey,
